6c43f84132479fccae235800876e2cec78373bd9
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-08-29 19:18:29 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -10,8 +10,8 @@ exports.run = (client, message, params) => {
   .setColor(0x00ffff)
   .addField("**Eğlence ve Kullanıcı Komutları:**", `şakir-banned = Dene ve Gör! \nşakir-avatarım = Avatarınınızı Gösterir. \nşakir-herkesebendençay = Herkese Çay Alırsınız. \nşakir-koş = Koşarsınız.\nşakir-çayiç = Çay İçersiniz. \nşakir-çekiç = İstediğiniz Kişiye Çekiç Atarsınız. \nşakir-çayaşekerat = Çaya Şeker Atarsınız. \nşakir-yumruh-at = Yumruk Atarsınız. \nşakir-yaz = Bota İstediğiniz Şeyi Yazdırırsınız. \nşakir-sunucuresmi = BOT Sunucunun Resmini Atar. \nşakir-sunucubilgi = BOT Sunucu Hakkında Bilgi Verir. \nşakir-kullanıcıbilgim = Sizin Hakkınızda Bilgi Verir. \nşakir-söv = Size Özelden Söver.`)
   .addField("**Sunucu Yetkilisi Komutları**", `şakir-ban = İstediğiniz Kişiyi Sunucudan Banlar. \nşakir-kick  = İstediğiniz Kişiyi Sunucudan Atar. \nşakir-unban = İstediğiniz Kişinin Yasağını Açar. \nşakir-sustur = İstediğiniz Kişiyi Susturur. -MUTED- \nşakir-oylama = Oylama Açar. \nşakir-duyurbot = Güzel Bir Duyuru Görünümü Sağlar.`)
-  .addField("**Botun Ana Komutları**", "şakir-yardım = BOT Komutlarını Atar. \şakir-bilgi = BOT Kendisi Hakkında Bilgi Verir. \nşakir-ping = BOT Gecikme Süresini Söyler. \nşakir-davet = BOT Davet Linkini Atar. \nk!istatistik = BOT İstatistiklerini Atar.")
-  .addField("**Gizli Komutlar:**", " **herkese çay = Dene Ve Gör \npeki şakire çay yok= Dene Ve Gör \nadınımı değiştirdin = Dene Ve Gör**\nne diyem mahmutmu diyem şakir = Dene Ve Gör\no günler bitti şakir= Dene Ve Gör\nşakir = Denede Gör\nşakir bi sus\ Dene Ve Gör   ")
+  .addField("**Botun Ana Komutları**", "şakir-yardım = BOT Komutlarını Atar. \şakir-bilgi = BOT Kendisi Hakkında Bilgi Verir. \nşakir-ping = BOT Gecikme Süresini Söyler. \nşakir-davet = BOT Davet Linkini Atar. \nk!istatistik = BOT İstatistiklerini Atar.**")
+  .addField("**Gizli Komutlar:**", " **herkese çay = Dene Ve Gör \npeki şakire çay yok= Dene Ve Gör \nadınımı değiştirdin = Dene Ve Gör\nne diyem mahmutmu diyem şakir = Dene Ve Gör\no günler bitti şakir= Dene Ve Gör\nşakir = Denede Gör\nşakir bi sus\ Dene Ve Gör   ")
   .setFooter('**Beni Yaratan =ＬＥＶＹＥ#8336**  ')
 
 
