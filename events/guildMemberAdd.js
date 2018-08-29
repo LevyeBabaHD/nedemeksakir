d77ee178dd872dfc53f691f04a1d858c0d5fef9e
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-08-29 18:12:49 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,19 @@
-module.exports = member => {
-    let username = member.user.username;
-    member.send('Sunucuya Hoş Geldin Bu Sunucuda Şakir Kullanılıyor!');
-    member.guild.default.send('');
-};
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find('name', 'hosgeldin');
+  if (!channel) return;
+    const hg = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setThumbnail(member.user.displayAvatarURL)
+    .setDescription('Sunucuya yeni biri katıldı hoşgeldin! ' + `${member}`);
+    return channel.sendEmbed(hg);
+});
+
+client.on('guildMemberRemove', member => {
+  const channel = member.guild.channels.find('name', 'hosgeldin');
+  if (!channel) return;
+     const bb = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setThumbnail(member.user.displayAvatarURL)
+    .setDescription('Sunucudan ' + `${member} ` + 'ayrıldı güle güle!');
+    return channel.sendEmbed(bb);
+});
