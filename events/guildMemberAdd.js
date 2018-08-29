c82eafc8ac7894816c22627709cd2173da5b14e1
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-08-29 17:57:46 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = member => {
     let username = member.user.username;
-    member.send('Sunucuya Hoş Geldin!');
-    member.guild.defaultChannel.send('');
+    member.send('Sunucuya Hoş Geldin Bu Sunucuda Şakir Kullanılıyor!');
+    member.guild.default.send('');
 };
