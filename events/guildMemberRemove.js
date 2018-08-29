bf2f913690d62995ebb71023211b0e6a7b48ce36
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-08-29 17:58:40 +0300

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,5 +1,5 @@
 module.exports = member => {
   let guild = member.guild;
-  member.send('Seni Özleyeceğiz!');
-  guild.defaultChannel.send(``);
+  member.send('Seni Özleyeceğiz Bu Serverde Şakir Kullanılıyordu !');
+  guild.default.send(``);
 };
