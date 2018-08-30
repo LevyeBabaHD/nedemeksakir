3bd6e1c9bbf556461e9255aa5fcda7d01b855d68
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-08-30 14:31:00 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,5 +9,5 @@ module.exports = client => {
   console.log([${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!);
   console.log([${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!);
   client.user.setStatus("online");
-client.user.setGame(" 30 Ağustos Zafer Bayramı kutlu olsun!", "https://twitch.tv/scarew0%22);
+client.user.setGame(" 30 Ağustos Zafer Bayramı kutlu olsun!", "https://twitch.tv/grmemo);
 }
