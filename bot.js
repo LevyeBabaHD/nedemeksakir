5dbd510de971405ebef980a9ce86427a70184480
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-08-30 14:50:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,6 +27,30 @@ fs.readdir('./komutlar/', (err, files) => {
   });
 });
 
+client.on('guildMemberAdd', member => {
+  const channel = member.guild.channels.find('name', 'hosgeldin');
+  if (!channel) return;
+    const hg = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setThumbnail(member.user.displayAvatarURL)
+    .setDescription('Sunucuya yeni biri katıldı hoşgeldin! ' + ${member});
+    return channel.sendEmbed(hg);
+});
+
+client.on('guildMemberRemove', member => {
+  const channel = member.guild.channels.find('name', 'hosgeldin');
+  if (!channel) return;
+     const bb = new Discord.RichEmbed()
+    .setColor('RANDOM')
+    .setThumbnail(member.user.displayAvatarURL)
+    .setDescription('Sunucudan ' + ${member} + 'ayrıldı güle güle!');
+    return channel.sendEmbed(bb);
+});
+
+
+
+
+
 client.reload = command => {
   return new Promise((resolve, reject) => {
     try {
