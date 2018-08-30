b28b5fac24faf151b7e3711b5113c655bd0e2e15
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-08-31 00:38:21 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,5 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-  client.user.setGame(`30 AĞUSTOS ZAFER BAYRAMINIZ KUTLU OLSUN `);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
-  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
-};
+client.user.setGame("şakir-yardım | Yeni:Duyurbot Sadece Kurucular Kullanabilir |  " + client.guilds.size + " Sunucu " + client.users.size + " Kullanıcı ", "https://twitch.tv/grmemo");
+}
