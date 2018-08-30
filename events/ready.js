632431be5ffebd51f81bd3d5b3d1227c5da10443
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-08-30 14:37:40 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,4 +9,5 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-client.user.setGame("30 Ağustos Zafer Bayramı kutlu olsun!",)
+client.user.setGame("30 AĞUSTOS ZAFER BAYRAMINIZ KUTLU OLSUN |
+}
