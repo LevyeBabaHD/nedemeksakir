83fb6e925546f38d2aa4a9e8980dc5d95d8bc3d9
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-09-01 09:50:36 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,5 +9,5 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-client.user.setGame("şakir-yardım | Yeni:Duyurbot Sadece Kurucular Kullanabilir |  " + client.guilds.size + " Sunucu " + client.users.size + " Kullanıcı ", "https://twitch.tv/grmemo");
+client.user.setGame("şakir-yardım | Küfür Engeli Reklam Engeli Aktif |  " + client.guilds.size + " Sunucu " + client.users.size + " Kullanıcı ", "https://twitch.tv/grmemo");
 }
