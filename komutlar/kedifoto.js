f39a7fc2b2ee70046b7020b6bcd7871d6f47bb4a
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-09-01 22:08:21 +0300

    Add files via upload

---
+++ komutlar/kedifoto.js
@@ -0,0 +1,31 @@
+﻿const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+exports.run = (client, message, args) => {
+  message.channel.send(':clock1: | Fotoğraf Yükleniyor..')
+  kedifoto1 = "./img/1.jpg"; kedifoto2 = "./img/2.jpg"; kedifoto3 = "./img/3.jpg"; kedifoto4 = "./img/4.png"; kedifoto5 = "./img/5.jpg"; kedifoto6 = "img/6.jpg";
+    number = 3,2,4,1,5,6
+    var random = Math.floor(Math.random() * (number - 1 + 1)) + 1; 
+    switch(random) {
+        case 1: message.channel.send({ files: [kedifoto1] }); message.channel.bulkDelete(1); break;
+        case 2: message.channel.send({ files: [kedifoto2] }); message.channel.bulkDelete(1); break;
+        case 3: message.channel.send({ files: [kedifoto3] }); message.channel.bulkDelete(1); break;
+        case 4: message.channel.send({ files: [kedifoto4] }); message.channel.bulkDelete(1); break;
+        case 5: message.channel.send({ files: [kedifoto5] }); message.channel.bulkDelete(1); break;
+        case 6: message.channel.send({ files: [kedifoto6] }); message.channel.bulkDelete(1); break;
+};
+};
+
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['kedifoto', 'kedi'],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'kedi',
+  description: 'Rastgele Bir Şekilde Kedi Fotoğrafı Yollar.!',
+  usage: 'kedi'
+};
