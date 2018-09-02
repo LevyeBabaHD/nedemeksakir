b52b3e9406f9ef6a496947b048c7b172b2bf40f2
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-09-02 19:29:49 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,5 +9,5 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-client.user.setGame("şakir-yardım | Küfür Engeli Reklam Engeli Aktif |  " + client.guilds.size + " Sunucu " + client.users.size + " Kullanıcı ", "https://twitch.tv/grmemo");
+client.user.setGame("şakir-yardım | Yeni Haraketli şakir-yardım Geldi <3PRONAX<3 <3ŞAKİR<3 |  " + client.guilds.size + " Sunucu " + client.users.size + " Kullanıcı ", "https://twitch.tv/grmemo");
 }
