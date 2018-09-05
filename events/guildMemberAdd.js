d68a6baa85a10f8fd948c89f14ee5a10816d311a
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-09-05 14:24:22 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,9 +1,5 @@
-client.on('guildMemberAdd', member => {
-  const channel = member.guild.channels.find('name', 'hosgeldin');
-  if (!channel) return;
-    const hosgeldin = new Discord.RichEmbed()
-    .setColor('RANDOM')
-    .setThumbnail(member.user.displayAvatarURL)
-    .setDescription('Sunucuya yeni biri katıldı hoşgeldin! ' + `${member}`);
-    return channel.sendEmbed(hosgeldin);
-});
+	module.exports = member => {
+	  let guild = member.guild;
+	  member.send('Seni Özleyeceğiz!');
+	  guild.defaultChannel.send(`bb`);
+	};
