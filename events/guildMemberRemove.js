25e9ef6160fa89df775902dd66c7c7c81b09f8ec
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-09-05 14:21:18 +0300

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,9 +1,5 @@
-client.on('guildMemberRemove', member => {
-  const channel = member.guild.channels.find('name', 'hosgeldin');
-  if (!channel) return;
-     const gülegüle = new Discord.RichEmbed()
-    .setColor('RANDOM')
-    .setThumbnail(member.user.displayAvatarURL)
-    .setDescription('Sunucudan ' + `${member} ` + 'ayrıldı güle güle!');
-    return channel.sendEmbed(gülegüle);
-});
+module.exports = member => {
+  let guild = member.guild;
+  member.send('Seni Özleyeceğiz!');
+  guild.defaultChannel.send(`bb`);
+};
