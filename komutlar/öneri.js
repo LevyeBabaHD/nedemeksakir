34534daf2d899cb6cddb6d3cdb6c3a5e96e12ea4
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-09-07 10:43:10 +0300

    Create öneri.js

---
+++ komutlar/öneri.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+
+exports.run = function(client, message, args) {
+
+    var öneri = args.slice(0).join(' ');
+    var guildID = "487194840908431361"; 
+    var channelID = "487524900437753856";
+
+    if (!öneri)  {
+        return message.reply("Lütfen Bir Mesaj Belirtiniz! Kullanım: <öneri <öneriniz> .");
+    } else {
+
+        var embed = new Discord.RichEmbed()
+            .setTimestamp()
+            .addField("Kullanıcı: " + message.author.tag, true)
+            .addField("Kullanıcı ID: " + message.author.id, true)
+            .addField("Öneri: " + öneri, true)
+
+        client.guilds.get(guildID).channels.get(channelID).send(embed);
+        message.reply(":clap: Teşekkürler! Bildirimiz Admine İletildi");
+    };
+};
+
+exports.conf = {
+ enabled: true,
+ guildOnly: false,
+ aliases: ["önerim", "öneri", "önerilerim", "onerim", "onerilerim"],
+ permLevel: 0
+};
+
+exports.help = {
+ name: 'öneri',
+ description: 'Önerinizi Admine Bildirir! ',
+ usage: 'öneri'
+};
