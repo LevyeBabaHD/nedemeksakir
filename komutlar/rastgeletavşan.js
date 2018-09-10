994b07fcd637e6ebe4ee839a0eab9784e1f018d3
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-09-10 12:37:21 +0300

    Add files via upload

---
+++ komutlar/rastgeletavşan.js
@@ -0,0 +1,93 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const request = require("request");
+
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  let imgs = Math.floor(Math.random() * 80);
+  let url = ['https://www.reddit.com/r/Rabbits/.json?sort=rising&t=hour&limit=100'];
+  request({
+    method: 'GET',
+    uri: url[Math.floor(Math.random() * url.length)]
+  }, function (err, response, data) {
+    if(err) {
+      console.log(err, null);
+      return;
+    }
+
+  data = JSON.parse(data);
+  var mainObj = data.data.children;
+  var urls = {};
+
+  for(let i = 0; i < mainObj.length; i++) {
+  let url = mainObj[i].data.url;
+  urls[i+1] = url;
+    }
+  const embed = new Discord.RichEmbed()
+  .setTitle("Jump!")
+  .setColor(0xC93457)
+  .setImage(urls[imgs])
+  message.channel.send({embed});
+
+  if(client.user && message.content === "undefined") {
+      message.delete()
+  }})
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["rastgeletavşan"],
+  permLevel: "0"
+};
+
+exports.help = {
+  name: "rastgeletavşan",
+  category: "Eğlence",
+  description: "Jumpy I go",
+  usage: "rastgeletavşan"
+};
+
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  let imgs = Math.floor(Math.random() * 80);
+  let url = ['https://www.reddit.com/r/Rabbits/.json?sort=rising&t=hour&limit=100'];
+  request({
+    method: 'GET',
+    uri: url[Math.floor(Math.random() * url.length)]
+  }, function (err, response, data) {
+    if(err) {
+      console.log(err, null);
+      return;
+    }
+
+  data = JSON.parse(data);
+  var mainObj = data.data.children;
+  var urls = {};
+
+  for(let i = 0; i < mainObj.length; i++) {
+  let url = mainObj[i].data.url;
+  urls[i+1] = url;
+    }
+  const embed = new Discord.RichEmbed()
+  .setTitle("Zıpla! Zıpla!")
+  .setColor(0xC93457)
+  .setImage(urls[imgs])
+  message.channel.send({embed});
+
+  if(client.user && message.content === "undefined") {
+      message.delete()
+  }})
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["bunny"],
+  permLevel: "0"
+};
+
+exports.help = {
+  name: "bunny",
+  category: "Miscelaneous",
+  description: "Jumpy I go",
+  usage: "bunny"
+};
