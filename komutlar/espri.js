9510ff56c5b4f422204e4f225b25a94dbfd4fa68
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-09-16 01:14:04 +0300

    Delete espri.js

--- komutlar/espri.js
+++
@@ -1,9 +0,0 @@
-var request = require('request');
-
-request('https://api.eggsybot.xyz/espri', function (error, response, body) {
-    if (error) return console.log('Hata:', error); // Hata olursa, konsola göndersin,
-    else if (!error) { // Eğer hata yoksa;
-        var info = JSON.parse(body); // info değişkeninin içerisine JSON'ı ayrıştırsın,
-        console.log(info.soz); // ve konsola çıktıyı versin.
-    }
-});
