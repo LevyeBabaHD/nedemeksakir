7df7eee26185dcb44327cdd7e607c6902427a11b
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-09-15 17:42:22 +0300

    Create atom.js

---
+++ komutlar/atom.js
@@ -0,0 +1,34 @@
+  const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+exports.run = (client, message, params) => {
+	if (!message.guild) {
+    const ozelmesajuyari = new Discord.RichEmbed()
+    .setColor(0xFF0000)
+    .setTimestamp()
+    .setAuthor(message.author.username, message.author.avatarURL)
+    .addField('Atom')
+    return message.author.sendEmbed(ozelmesajuyari); }
+    if (message.channel.type !== '..') {
+      const sunucubilgi = new Discord.RichEmbed()
+    .setAuthor('**Buraları Yok Ettin!**')
+    .setColor(3447003)
+    .setTimestamp()
+    .setDescription('')
+		.setImage(`https://media.giphy.com/media/XevXoNu5WZxe0/giphy.gif`)
+    return message.channel.sendEmbed(sunucubilgi);
+    }
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 0
+};
+
+exports.help = {
+  name: 'atom',
+  description: 'Atom Bombası Atarsınız.',
+  usage: 'atom'
+};
