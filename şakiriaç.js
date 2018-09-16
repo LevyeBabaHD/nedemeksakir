8863a1e0f7e9c789300269b1f8aa8ff15ce5a81f
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-09-16 11:59:45 +0300

    Update şakiriaç.js

--- şakiriaç.js
+++ şakiriaç.js
@@ -243,7 +243,15 @@ client.on('message', msg => {
 
 
 
+var request = require('request');
 
+request('https://api.eggsybot.xyz/espri', function (error, response, body) {
+    if (error) return console.log('Hata:', error);
+    else if (!error) {
+        var info = JSON.parse(body);
+        console.log(info.soz);
+    }
+});
 
 client.elevation = message => {
   if(!message.guild) {
