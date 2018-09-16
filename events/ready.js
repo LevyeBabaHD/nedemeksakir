b719aa81e54e9cd080a6a315bac14585995814b0
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-09-16 12:03:22 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -8,5 +8,6 @@ var prefix = ayarlar.prefix;
 module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setGame("şakir-yardım | Bütün Yenilikler şakir-yardım ın Son Sayfasına Eklendi |  " + client.guilds.size + " Sunucu " + client.users.size + " Kullanıcı ", "https://www.twitch.tv/grmemo%22);
   client.user.setStatus("online");
 }
