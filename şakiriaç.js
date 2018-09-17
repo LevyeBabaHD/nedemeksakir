fac5b83b1f93c6641cd571929d136880a2f807a0
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-09-17 13:39:28 +0300

    Update şakiriaç.js

--- şakiriaç.js
+++ şakiriaç.js
@@ -143,6 +143,66 @@ client.on('message', msg => {
   }
 });
   
+
+
+
+client.on("message", async message => {
+  var user = message.mentions.users.first() || message.author;
+    if (message.content.toLowerCase() === prefix + "sniper") {
+        var user = message.mentions.users.first() || message.author;
+        if (!message.guild) user = message.author;
+
+        message.channel.send("⏲ | `Profil Fotoğrafınıza` **Göre Ayarlıyorum. Bu Biraz Zaman Alabilir**").then(m => m.delete(1000));
+
+        Jimp.read(user.avatarURL, (err, image) => {
+            image.resize(310, 325)
+            image.greyscale()
+            image.gaussian(3)
+            Jimp.read("https://cdn.glitch.com/b18a2fa6-68cb-49d5-9818-64c50dd0fdab%2FPNGPIX-COM-Crosshair-PNG-Transparent-Image.png?1529363625811", (err, avatar) => {
+                avatar.resize(310, 325)
+                image.composite(avatar, 2, 0).write(`./img/snip/${client.user.id}-${user.id}.png`);
+                setTimeout(function() {
+                    message.channel.send(new Discord.Attachment(`./img/snip/${client.user.id}-${user.id}.png`));
+                }, 1000);
+            });
+
+        });
+    }
+});
+
+
+client.on("message", async message => {
+  var user = message.mentions.users.first() || message.author;
+    if (message.content.toLowerCase() === prefix + "hypesquad") {
+        var user = message.mentions.users.first() || message.author;
+        if (!message.guild) user = message.author;
+
+        message.channel.send("⏲ | `Profil Fotoğrafınıza` **Göre Ayarlıyorum. Bu Biraz Zaman Alabilir**").then(m => m.delete(1000));
+   var b = [`https://raw.githubusercontent.com/whitskin/gizliyav/master/img/brilliance/brilliance1.png`]
+  var img = b[Math.floor(Math.random() * b.length)]
+        Jimp.read(user.avatarURL, (err, image) => {
+            image.resize(310, 325)
+            image.greyscale()
+            image.gaussian(3)
+            Jimp.read(img, (err, avatar) => {
+                avatar.resize(310, 325)
+                image.composite(avatar, 2, 0).write(`./img/snip/${client.user.id}-${user.id}.png`);
+                setTimeout(function() {
+                    message.channel.send(new Discord.Attachment(`./img/snip/${client.user.id}-${user.id}.png`));
+                }, 1000);
+            });
+
+        });
+    }
+});
+
+
+
+
+
+
+
+
 const snekfetch = require('snekfetch')
 
 setInterval(() => {
