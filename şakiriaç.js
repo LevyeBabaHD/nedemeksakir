e337dd3bc28d93690ccbe253fc57e4563710953e
LevyeBabaHD <42775775+LevyeBabaHD@users.noreply.github.com>
2018-09-17 12:49:21 +0300

    Update şakiriaç.js

--- şakiriaç.js
+++ şakiriaç.js
@@ -143,6 +143,19 @@ client.on('message', msg => {
   }
 });
   
+const snekfetch = require('snekfetch')
+const dbotstoken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQ4NDA1MjE4Mjk3NTY0MzY0OSIsImJvdCI6dHJ1ZSwiaWF0IjoxNTM2NjAwMTQxfQ.OS2TXNujsmH0FpxNbUjdqttoEM1DFLPbDe9ZSzaO2EY"
+
+
+
+client.shard.fetchClientValues('guilds.size').then(result => {
+const guildsizes = result.reduce((prev, val) => prev + val, 0)
+    snekfetch.post(`https://discordbots.org/api/bots/${client.user.id}/stats`)
+        .set('Authorization', dbotstoken)
+        .send({"server_count": guildsizes})
+        .then(() => console.log(`dbotsstats updated successfully`))
+        .catch(err => console.error(err))
+})
 
 client.reload = command => {
   return new Promise((resolve, reject) => {
